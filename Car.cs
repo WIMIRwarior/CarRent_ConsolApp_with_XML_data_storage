d1d9bfa202f61c260c7e2210442aee00f06616ad
Karol_Czernecki <inz.karol.czernecki@gmail.com>
2025-10-29 17:56:45 +0100

    Dodaj pliki projektów.

---
+++ Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentConsoleAppWithXML
+{
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private string year_of_production;
+        private double mileage;
+        private string VIN;
+        private bool available;
+        private double rent_fee;
+
+        public string getBrand()
+        { return brand; }
+        public string getModel() { return model; }
+        public string getYear_of_production() { return year_of_production; }
+        public double getMileage() { return mileage; }
+        public double getRent_fee() { return rent_fee; }
+        public string getVIN() { return VIN; }
+        public bool isAvailable() { return available; }
+
+        public void setAvailability(bool availability)
+        { 
+            this.available = availability; 
+        }
+
+        public void updateMileage(double kilometers)
+        {
+            this.mileage += kilometers;
+        }
+
+        public void setRent_Fee(double fee)
+        {
+            this.rent_fee = fee;
+        }
+
+        public Car(string brand, string model, string year_of_production, double mileage, string VIN, bool availability,double rentFee)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.year_of_production = year_of_production;
+            this.mileage = mileage;
+            this.VIN = VIN;
+            this.available = availability;
+            this.rent_fee = rentFee;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace CarRentConsoleAppWithXML
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Start();
+            Loop();
+            read_from_XML_file();
+
+        }
+
+        private static void Start()
+        {
+
+        }
+
+        private static void Loop()
+        {
+
+        }
+
+        private static void write_to_XML_file()
+        {
+
+        }
+
+        private static void read_from_XML_file()
+        { 
+            XmlDocument xmlDoc = new XmlDocument();
+            var FullPath = Path.Combine(Directory.GetCurrentDirectory(), "Data_Storage.xml");
+            XmlNodeList xNodeList;
+            string? str = null;
+            FileStream FS_Open_Read = new FileStream(FullPath,FileMode.Open, FileAccess.Read);
+            xmlDoc.Load(FS_Open_Read);
+            xNodeList = xmlDoc.GetElementsByTagName("car");
+
+            for(int i=0;i<xNodeList.Count;i++)
+            {
+                Console.WriteLine("===============");
+                Console.WriteLine("CAR_"+(i+1));
+                Console.WriteLine("===============");
+
+
+                for (int j=0;j<7;j++)
+                {
+                    str = xNodeList[i].ChildNodes.Item(j).InnerText.Trim();
+                    Console.WriteLine(str);
+                }
+            }
+        }
+
+    }
+
+
+
+
+
+}
